c06e72f8698825b95a0954b88b5fdd436beeef9a
Raul Pedrosa <sr.raulgp@gmail.com>
2022-10-08 23:22:50 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero1 = 5;
+        int numero2 = 10;
+        int resultado = numero1 + numero2;
+        System.out.println("A soma entre os dois números é: " + resultado);
+    }
+}
